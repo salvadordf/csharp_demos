1e327efcbe1e51d40c909eddad4912f705068545
Salvador Díaz Fau <briskbard@briskbard.com>
2023-11-15 21:45:14 +0100

    Added the WebView2WinForms demo

--- WebView2WinForms/WebView2WinForms/MainForm.Designer.cs
+++ WebView2WinForms/WebView2WinForms/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace CefSharpWinForms
+﻿namespace WebView2WinForms
 {
     partial class MainForm
     {
@@ -30,21 +30,21 @@
         {
             components = new System.ComponentModel.Container();
             PnlNavControl = new Panel();
+            CbxURL = new ComboBox();
             PnlConfig = new Panel();
             BtnConfig = new Button();
             BtnGo = new Button();
-            CbxURL = new ComboBox();
             PnlNavButton = new Panel();
             BtnStop = new Button();
             BtnReload = new Button();
             BtnForward = new Button();
             BtnBack = new Button();
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
             statusStrip1 = new StatusStrip();
             toolStripStatusLabel1 = new ToolStripStatusLabel();
             toolStripStatusLabel2 = new ToolStripStatusLabel();
-            chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
             contextMenuStrip1 = new ContextMenuStrip(components);
-            showDevToolsToolStripMenuItem = new ToolStripMenuItem();
+            dEvToolsToolStripMenuItem = new ToolStripMenuItem();
             printToPDFToolStripMenuItem = new ToolStripMenuItem();
             openFileToolStripMenuItem = new ToolStripMenuItem();
             toolStripMenuItem1 = new ToolStripSeparator();
@@ -54,6 +54,7 @@
             PnlNavControl.SuspendLayout();
             PnlConfig.SuspendLayout();
             PnlNavButton.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
             statusStrip1.SuspendLayout();
             contextMenuStrip1.SuspendLayout();
             SuspendLayout();
@@ -61,14 +62,24 @@
             // PnlNavControl
             // 
             PnlNavControl.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            PnlNavControl.Controls.Add(PnlConfig);
             PnlNavControl.Controls.Add(CbxURL);
+            PnlNavControl.Controls.Add(PnlConfig);
             PnlNavControl.Controls.Add(PnlNavButton);
             PnlNavControl.Location = new Point(0, 0);
-            PnlNavControl.Margin = new Padding(0);
             PnlNavControl.Name = "PnlNavControl";
             PnlNavControl.Size = new Size(1008, 25);
             PnlNavControl.TabIndex = 0;
+            // 
+            // CbxURL
+            // 
+            CbxURL.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            CbxURL.FormattingEnabled = true;
+            CbxURL.Items.AddRange(new object[] { "https://www.bing.com", "https://www.google.com", "https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending", "https://www.w3schools.com/js/tryit.asp?filename=tryjs_win_close", "https://www.w3schools.com/js/tryit.asp?filename=tryjs_alert", "https://www.w3schools.com/js/tryit.asp?filename=tryjs_loc_assign", "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_style_backgroundcolor", "https://www.w3schools.com/Tags/tryit.asp?filename=tryhtml_iframe_name", "https://www.w3schools.com/html/html5_video.asp", "http://html5test.com/", "https://webrtc.github.io/samples/src/content/devices/input-output/", "https://test.webrtc.org/", "https://www.browserleaks.com/webrtc", "https://shaka-player-demo.appspot.com/demo/", "http://webglsamples.org/", "https://get.webgl.org/", "https://www.briskbard.com", "https://www.youtube.com", "https://html5demos.com/drag/", "https://frames-per-second.appspot.com/", "https://badssl.com/", "https://www.httpwatch.com/httpgallery/authentication/", "https://www.sede.fnmt.gob.es/certificados/persona-fisica/verificar-estado", "https://badssl.com/", "edge://flags/", "edge://gpu/", "edge://about/" });
+            CbxURL.Location = new Point(120, 0);
+            CbxURL.Name = "CbxURL";
+            CbxURL.Size = new Size(821, 23);
+            CbxURL.TabIndex = 2;
+            CbxURL.Text = "https://www.briskbard.com";
             // 
             // PnlConfig
             // 
@@ -78,40 +89,29 @@
             PnlConfig.Location = new Point(943, 0);
             PnlConfig.Name = "PnlConfig";
             PnlConfig.Size = new Size(65, 25);
-            PnlConfig.TabIndex = 2;
+            PnlConfig.TabIndex = 1;
             // 
             // BtnConfig
             // 
-            BtnConfig.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            BtnConfig.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             BtnConfig.Location = new Point(35, 0);
             BtnConfig.Name = "BtnConfig";
             BtnConfig.Size = new Size(25, 25);
-            BtnConfig.TabIndex = 1;
+            BtnConfig.TabIndex = 5;
             BtnConfig.Text = "≡";
             BtnConfig.UseVisualStyleBackColor = true;
             BtnConfig.Click += BtnConfig_Click;
             // 
             // BtnGo
             // 
-            BtnGo.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            BtnGo.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             BtnGo.Location = new Point(5, 0);
             BtnGo.Name = "BtnGo";
             BtnGo.Size = new Size(25, 25);
-            BtnGo.TabIndex = 0;
+            BtnGo.TabIndex = 4;
             BtnGo.Text = "►";
             BtnGo.UseVisualStyleBackColor = true;
             BtnGo.Click += BtnGo_Click;
-            // 
-            // CbxURL
-            // 
-            CbxURL.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            CbxURL.FormattingEnabled = true;
-            CbxURL.Items.AddRange(new object[] { "https://www.google.com", "https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending", "https://www.w3schools.com/js/tryit.asp?filename=tryjs_win_close", "https://www.w3schools.com/js/tryit.asp?filename=tryjs_alert", "https://www.w3schools.com/js/tryit.asp?filename=tryjs_loc_assign", "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_style_backgroundcolor", "https://www.w3schools.com/Tags/tryit.asp?filename=tryhtml_iframe_name", "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_input_type_file", "https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_state_throw_error", "https://www.htmlquick.com/es/reference/tags/input-file.html", "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file", "https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/webkitdirectory", "https://www.w3schools.com/html/html5_video.asp", "http://html5test.com/", "https://webrtc.github.io/samples/src/content/devices/input-output/", "https://test.webrtc.org/", "https://www.browserleaks.com/webrtc", "https://shaka-player-demo.appspot.com/demo/", "http://webglsamples.org/", "https://get.webgl.org/", "https://www.briskbard.com", "https://www.youtube.com", "https://html5demos.com/drag/", "https://frames-per-second.appspot.com/", "https://www.sede.fnmt.gob.es/certificados/persona-fisica/verificar-estado", "https://www.kirupa.com/html5/accessing_your_webcam_in_html5.htm", "https://www.xdumaine.com/enumerateDevices/test/", "https://dagrs.berkeley.edu/sites/default/files/2020-01/sample.pdf", "chrome://version/", "chrome://net-internals/", "chrome://tracing/", "chrome://appcache-internals/", "chrome://blob-internals/", "chrome://view-http-cache/", "chrome://credits/", "chrome://histograms/", "chrome://media-internals/", "chrome://kill", "chrome://crash", "chrome://hang", "chrome://shorthang", "chrome://gpuclean", "chrome://gpucrash", "chrome://gpuhang", "chrome://extensions-support", "chrome://process-internals" });
-            CbxURL.Location = new Point(120, 0);
-            CbxURL.Name = "CbxURL";
-            CbxURL.Size = new Size(821, 23);
-            CbxURL.TabIndex = 1;
-            CbxURL.Text = "https://www.briskbard.com";
             // 
             // PnlNavButton
             // 
@@ -120,14 +120,13 @@
             PnlNavButton.Controls.Add(BtnForward);
             PnlNavButton.Controls.Add(BtnBack);
             PnlNavButton.Location = new Point(0, 0);
-            PnlNavButton.Margin = new Padding(2, 1, 2, 1);
             PnlNavButton.Name = "PnlNavButton";
             PnlNavButton.Size = new Size(120, 25);
             PnlNavButton.TabIndex = 0;
             // 
             // BtnStop
             // 
-            BtnStop.Font = new Font("Webdings", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            BtnStop.Font = new Font("Webdings", 12F);
             BtnStop.Location = new Point(90, 0);
             BtnStop.Name = "BtnStop";
             BtnStop.Size = new Size(25, 25);
@@ -138,7 +137,7 @@
             // 
             // BtnReload
             // 
-            BtnReload.Font = new Font("Webdings", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            BtnReload.Font = new Font("Webdings", 12F);
             BtnReload.Location = new Point(60, 0);
             BtnReload.Name = "BtnReload";
             BtnReload.Size = new Size(25, 25);
@@ -149,7 +148,7 @@
             // 
             // BtnForward
             // 
-            BtnForward.Font = new Font("Webdings", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            BtnForward.Font = new Font("Webdings", 12F);
             BtnForward.Location = new Point(30, 0);
             BtnForward.Name = "BtnForward";
             BtnForward.Size = new Size(25, 25);
@@ -160,7 +159,7 @@
             // 
             // BtnBack
             // 
-            BtnBack.Font = new Font("Webdings", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            BtnBack.Font = new Font("Webdings", 12F);
             BtnBack.Location = new Point(0, 0);
             BtnBack.Name = "BtnBack";
             BtnBack.Size = new Size(25, 25);
@@ -169,13 +168,30 @@
             BtnBack.UseVisualStyleBackColor = true;
             BtnBack.Click += BtnBack_Click;
             // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Location = new Point(0, 30);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(1006, 674);
+            webView21.Source = new Uri("https://www.briskbard.com/", UriKind.Absolute);
+            webView21.TabIndex = 1;
+            webView21.ZoomFactor = 1D;
+            webView21.CoreWebView2InitializationCompleted += webView21_CoreWebView2InitializationCompleted;
+            webView21.NavigationStarting += webView21_NavigationStarting;
+            webView21.NavigationCompleted += webView21_NavigationCompleted;
+            webView21.SourceChanged += webView21_SourceChanged;
+            // 
             // statusStrip1
             // 
             statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, toolStripStatusLabel2 });
             statusStrip1.Location = new Point(0, 707);
             statusStrip1.Name = "statusStrip1";
             statusStrip1.Size = new Size(1008, 22);
-            statusStrip1.TabIndex = 1;
+            statusStrip1.TabIndex = 2;
             statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
@@ -190,31 +206,18 @@
             toolStripStatusLabel2.Name = "toolStripStatusLabel2";
             toolStripStatusLabel2.Size = new Size(500, 17);
             // 
-            // chromiumWebBrowser1
-            // 
-            chromiumWebBrowser1.ActivateBrowserOnCreation = false;
-            chromiumWebBrowser1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            chromiumWebBrowser1.Location = new Point(0, 31);
-            chromiumWebBrowser1.Name = "chromiumWebBrowser1";
-            chromiumWebBrowser1.Size = new Size(1006, 674);
-            chromiumWebBrowser1.TabIndex = 2;
-            chromiumWebBrowser1.AddressChanged += chromiumWebBrowser1_AddressChanged;
-            chromiumWebBrowser1.TitleChanged += chromiumWebBrowser1_TitleChanged;
-            chromiumWebBrowser1.LoadingStateChanged += chromiumWebBrowser1_LoadingStateChanged;
-            chromiumWebBrowser1.StatusMessage += chromiumWebBrowser1_StatusMessage;
-            // 
             // contextMenuStrip1
             // 
-            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { showDevToolsToolStripMenuItem, printToPDFToolStripMenuItem, openFileToolStripMenuItem, toolStripMenuItem1, exitToolStripMenuItem });
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { dEvToolsToolStripMenuItem, printToPDFToolStripMenuItem, openFileToolStripMenuItem, toolStripMenuItem1, exitToolStripMenuItem });
             contextMenuStrip1.Name = "contextMenuStrip1";
             contextMenuStrip1.Size = new Size(138, 98);
             // 
-            // showDevToolsToolStripMenuItem
+            // dEvToolsToolStripMenuItem
             // 
-            showDevToolsToolStripMenuItem.Name = "showDevToolsToolStripMenuItem";
-            showDevToolsToolStripMenuItem.Size = new Size(137, 22);
-            showDevToolsToolStripMenuItem.Text = "DevTools";
-            showDevToolsToolStripMenuItem.Click += showDevToolsToolStripMenuItem_Click;
+            dEvToolsToolStripMenuItem.Name = "dEvToolsToolStripMenuItem";
+            dEvToolsToolStripMenuItem.Size = new Size(137, 22);
+            dEvToolsToolStripMenuItem.Text = "DevTools";
+            dEvToolsToolStripMenuItem.Click += dEvToolsToolStripMenuItem_Click;
             // 
             // printToPDFToolStripMenuItem
             // 
@@ -227,7 +230,7 @@
             // 
             openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
             openFileToolStripMenuItem.Size = new Size(137, 22);
-            openFileToolStripMenuItem.Text = "Open file...";
+            openFileToolStripMenuItem.Text = "Open File...";
             openFileToolStripMenuItem.Click += openFileToolStripMenuItem_Click;
             // 
             // toolStripMenuItem1
@@ -257,16 +260,16 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1008, 729);
-            Controls.Add(chromiumWebBrowser1);
             Controls.Add(statusStrip1);
+            Controls.Add(webView21);
             Controls.Add(PnlNavControl);
-            Margin = new Padding(2, 1, 2, 1);
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "MiniBrowser";
             PnlNavControl.ResumeLayout(false);
             PnlConfig.ResumeLayout(false);
             PnlNavButton.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
             statusStrip1.ResumeLayout(false);
             statusStrip1.PerformLayout();
             contextMenuStrip1.ResumeLayout(false);
@@ -277,26 +280,26 @@
         #endregion
 
         private Panel PnlNavControl;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripStatusLabel toolStripStatusLabel2;
         private Panel PnlNavButton;
-        private Button BtnForward;
+        private Panel PnlConfig;
+        private ComboBox CbxURL;
         private Button BtnBack;
         private Button BtnStop;
         private Button BtnReload;
-        private ComboBox CbxURL;
-        private Panel PnlConfig;
-        private Button BtnConfig;
+        private Button BtnForward;
         private Button BtnGo;
-        private StatusStrip statusStrip1;
-        private ToolStripStatusLabel toolStripStatusLabel1;
-        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
-        private ToolStripStatusLabel toolStripStatusLabel2;
+        private Button BtnConfig;
         private ContextMenuStrip contextMenuStrip1;
-        private ToolStripMenuItem showDevToolsToolStripMenuItem;
         private ToolStripMenuItem printToPDFToolStripMenuItem;
+        private SaveFileDialog saveFileDialog1;
+        private OpenFileDialog openFileDialog1;
+        private ToolStripMenuItem dEvToolsToolStripMenuItem;
         private ToolStripMenuItem openFileToolStripMenuItem;
         private ToolStripSeparator toolStripMenuItem1;
         private ToolStripMenuItem exitToolStripMenuItem;
-        private SaveFileDialog saveFileDialog1;
-        private OpenFileDialog openFileDialog1;
     }
 }
---
+++ WebView2WinForms/WebView2WinForms/MainForm.cs
@@ -0,0 +1,127 @@
+namespace WebView2WinForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBack_Click(object sender, EventArgs e)
+        {
+            webView21.GoBack();
+        }
+
+        private void BtnForward_Click(object sender, EventArgs e)
+        {
+            webView21.GoForward();
+        }
+
+        private void BtnReload_Click(object sender, EventArgs e)
+        {
+            webView21.Reload();
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            webView21.Stop();
+        }
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            webView21.CoreWebView2.Navigate(CbxURL.Text);
+        }
+
+        private void webView21_SourceChanged(object sender, Microsoft.Web.WebView2.Core.CoreWebView2SourceChangedEventArgs e)
+        {
+            addURL(webView21.Source.ToString());
+        }
+
+        private void webView21_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            PnlNavControl.Enabled = true;
+            EnableNavigationButtons(false);
+        }
+
+        private void EnableNavigationButtons(bool isNavigating)
+        {
+            BtnBack.Enabled = webView21.CanGoBack;
+            BtnForward.Enabled = webView21.CanGoForward;
+            BtnStop.Enabled = isNavigating;
+            BtnReload.Enabled = !isNavigating;
+        }
+
+        private void webView21_NavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+            EnableNavigationButtons(true);
+        }
+
+        private void webView21_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            EnableNavigationButtons(false);
+        }
+
+        private void printToPDFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                webView21.CoreWebView2.PrintToPdfAsync(saveFileDialog1.FileName);
+            }
+        }
+
+        private void BtnConfig_Click(object sender, EventArgs e)
+        {
+            Point point = new Point(BtnConfig.Location.X, BtnConfig.Location.Y + BtnConfig.Size.Height);
+            contextMenuStrip1.Show(PnlConfig.PointToScreen(point));
+        }
+
+        private void dEvToolsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.CoreWebView2.OpenDevToolsWindow();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void addURL(string url)
+        {
+            if (CbxURL.Items.IndexOf(url) == -1)
+            {
+                CbxURL.Items.Add(url);
+            }
+            CbxURL.Text = url;
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    byte[] bytes = File.ReadAllBytes(openFileDialog1.FileName);
+                    String file = Convert.ToBase64String(bytes);
+                    String mimeType;
+                    if (openFileDialog1.FilterIndex == 1)
+                    {
+                        mimeType = @"text/html";
+                    }
+                    else
+                    {
+                        mimeType = @"application/pdf";
+                    }
+
+                    String uri = "data:" + mimeType + ";charset=utf-8;base64," + Uri.EscapeDataString(file);
+                    addURL(uri);
+                    webView21.CoreWebView2.Navigate(uri);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("There was an error opening that file.\n" + exception.Message,
+                        "File error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
